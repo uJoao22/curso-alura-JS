0263c4e4a0884b310aa06002f341c77bffc84b5d
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-07 10:59:54 -0300

    Trabalhando com variáveis

---
+++ JS_1/1-meuPrimeiroPrograma.js
@@ -0,0 +1,12 @@
+console.log("Este é meu primeiro programa: Trabalhando com variáveis")
+
+const idade = 26
+
+console.log("idade")
+console.log(idade)
+console.log(idade+2)
+console.log(idade-2)
+console.log(idade/2)
+
+const idadeSomada = idade+2
+console.log(idadeSomada)
