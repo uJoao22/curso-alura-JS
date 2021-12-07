eddf3a84735d768a605118ce5306f181ae4ac012
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-07 17:33:19 -0300

    Desafio: Criando um array de 2 dimensões, matriz

---
+++ JS_2/7-matriz.js
@@ -0,0 +1,6 @@
+const alunos = ['João', 'Juliana', 'Caio', 'Ana']
+const medias = [10, 8, 7.5, 9]
+
+let listaGeral = [alunos, medias]
+
+console.log(`A média de ${listaGeral[0][0]} é de: ${listaGeral[1][0]}`)
