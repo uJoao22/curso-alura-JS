eeee1bdfaa4b75688f4e44a9cc052ebf90449027
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-07 13:27:21 -0300

    Atribuições de Variavéis

---
+++ JS_1/5-atribuicaoVariaveis.js
@@ -0,0 +1,20 @@
+console.log("Trabalhando com variáveis")
+
+const primeiroNome = "João Pedro"
+const sobrenome = "Botelho"
+
+//console.log(nome + " " + sobrenome) //Separar variaveis concatenando espaço
+console.log(primeiroNome ,sobrenome) //Separar variaveis com a virgula
+
+console.log(`Meu nome é ${primeiroNome} ${sobrenome}`)
+
+let contador = 0
+contador = contador + 1
+
+const nomeCompleto = primeiroNome + " " +sobrenome
+console.log(nomeCompleto)
+
+let ano //Declarando variavel
+ano = 20 //Atribuindo valor
+ano = 18 //Reatribuindo o valor da variavel
+console.log(ano)
