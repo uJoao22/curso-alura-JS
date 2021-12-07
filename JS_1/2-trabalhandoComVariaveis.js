bb4d77ee8d0822ff1b49506480c27bfd27404d09
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-07 11:27:35 -0300

    Trabalhando com variáveis pt2

---
+++ JS_1/2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+//Console.log("Trabalhando com variáveis") - ERRO
+console.log("Trabalhando com variáveis")
+//JS é case sensitive
+
+const idade = 18
+const nome = "João"
+
+//Não fazer
+// mes = "Janeiro"
