dad556cf3b88504ed8c267fdda57a6aaede04e49
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-07 17:12:38 -0300

    Desafio: Juntando salas

---
+++ JS_2/6-juntandoSalas.js
@@ -0,0 +1,8 @@
+const salaPy = ['Melisa', 'Helena', 'Rodrigo']
+const salaJs = ['Ju', 'Leo', 'Raquel']
+
+//Juntando as salas usanndo concat
+//Atribuindo a um novo array os elementos de salaPy concatenando com os elementos de salaJs
+const salaGeral =salaPy.concat(salaJs)
+
+console.log(salaGeral)
