83c9833e0ce069b57dca73aad4f8d1b38eee1fc8
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-07 18:48:06 -0300

    Entendendo melhor funções callbacks

---
+++ JS_2/11-callbacks.js
@@ -0,0 +1,7 @@
+const nomes = ["Ana", "Ju", "Leo", "Paulo"]
+
+nomes.forEach(imprimeNomes) //Executando o forEach e chamando a função imprimeNomes
+
+function imprimeNomes(nome){
+    console.log(nome)
+}
