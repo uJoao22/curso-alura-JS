760af6b220b6998e4be21e825dc1bb1d903cb980
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-07 18:34:25 -0300

    Desafio: Média com ForEach

---
+++ JS_2/10-mediaComForEach.js
@@ -0,0 +1,15 @@
+const notasAlunos = [10, 6.5, 8, 7.5]
+let soma = 0
+
+//USANDO FUNÇÃO
+// notasAlunos.forEach(function(element){
+//     soma += element
+// })
+
+//USANDO ARROW FUNCTION
+notasAlunos.forEach(nota => {
+    soma += element
+})
+
+let media = soma/notasAlunos.length
+console.log(media)
