f954edf115b11d4ea10e5260558d011be02a9b51
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-07 11:43:28 -0300

    Operações Aritiméticas e Conversão de tipos

---
+++ JS_1/4-conversaoDeTipos.js
@@ -0,0 +1,13 @@
+console.log("Conversão de Tipos")
+
+console.log("Ano: "+2021)
+console.log("2" + "2")
+console.log(parseInt("2") + parseInt("2")) //Convertendo textos em núemros usando parseInt
+
+//Quando aplicado conversões matematicas a textos o js tenta converter para números e efetuar a operação,
+//caso o texto não seja um número ele irá retornar NaN
+console.log("7" / "2")
+console.log("João" / "2") //Not a Number
+
+console.log(6.5) //Número com ponto flutuante, casas decimais, declara usando .
+console.log(6,5)
