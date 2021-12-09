36b5332e88a64cef691c6010cacdd992982150c1
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-09 15:04:35 -0300

    Desafio Printar Infos e corrigindo erro

---
+++ JS_3/1-objetos.js
@@ -0,0 +1,7 @@
+//Estrutura de um objeto
+const cliente = {
+    nome: "João",
+    idade: "18",
+    cpf: "012.345.678-90",
+    email: "joaopaniciobotelho@hotmail.com"
+}
---
+++ JS_3/2-printarInfos.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome: "João",
+    idade: "18",
+    cpf: "012.345.678-90",
+    email: "joaopaniciobotelho@hotmail.com"
+}
+
+console.log(`Meu nome é: ${cliente.nome}, tenho ${cliente.idade} anos, e meu e-mail para contato é: ${cliente.email}`)
+
+console.log(cliente.cpf.substring(0, 3))
