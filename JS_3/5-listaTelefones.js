bb4fc6b338aaea71a0cf6cb48e9e472a39caff41
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-09 15:51:54 -0300

    Acessando arrays de um objeto

---
+++ JS_3/5-listaTelefones.js
@@ -0,0 +1,9 @@
+const cliente = {
+    nome: "João",
+    idade: "18",
+    cpf: "012.345.678-90",
+    email: "joaopaniciobotelho@hotmail.com",
+    celular: ["9 1234-5678", "9 8765-4321"]
+}
+
+cliente.celular.forEach(number => console.log(number))
