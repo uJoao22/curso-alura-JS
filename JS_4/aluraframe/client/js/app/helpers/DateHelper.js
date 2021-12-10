31a4f137bbba8c706ad5e3c73112024f8e5e0e09
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-10 17:55:57 -0300

    Criando a classe DateHelper

---
+++ JS_4/aluraframe/client/js/app/helpers/DateHelper.js
@@ -0,0 +1,9 @@
+class DateHelper {
+    dataParaTexto(data){
+        return data.getDate() +'/'+ (data.getMonth()+1) +'/'+ data.getFullYear()
+    }
+
+    textoParaData(texto){
+        return new Date(...texto.split('-').map((item,index) => item - index%2))
+    }
+}
