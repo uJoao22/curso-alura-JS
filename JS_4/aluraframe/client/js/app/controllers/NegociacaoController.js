45fe8bb9ee071d3ac652c15bf36c9a4d79096870
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-10 17:30:35 -0300

    Melhorando a conversão de data

--- JS_4/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JS_4/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -11,6 +11,13 @@ class NegociacaoController{
 
         event.preventDefault()
 
-        let data = new Date(this._inputData.value.split('-'))
+        let data = new Date(
+            ...this._inputData.value.split('-').map(function(item,index){
+                if(index == 1)
+                    return item - 1
+                return item
+                })
+        )
+        console.log(data)
     }
 }
