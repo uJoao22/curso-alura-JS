4bccf33ff229d64f8e352a3d526fdfa6851b8b5f
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-10 16:54:18 -0300

    Enviando os dados do controller para a model

--- JS_4/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JS_4/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -2,14 +2,20 @@ class NegociacaoController{
 
     constructor(){
         let $ = document.querySelector.bind(document)
-        this.inputData = $("#data")
-        this.inputQuantidade = $("#quantidade")
-        this.inputValor = $("#valor")
+        this._inputData = $("#data")
+        this._inputQuantidade = $("#quantidade")
+        this._inputValor = $("#valor")
     }
 
     adiciona(event){
 
         event.preventDefault()
 
+        //Enviando os dados pegos pelo controller para a classe model
+        let negociacao = new Negociacao(
+            this._inputData.value,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
     }
 }
