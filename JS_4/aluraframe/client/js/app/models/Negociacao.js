54655a2d5358c7d65a0062e7f441abd34b8f7880
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-10 16:00:39 -0300

    Aplicando programação defensiva

--- JS_4/aluraframe/client/js/app/models/Negociacao.js
+++ JS_4/aluraframe/client/js/app/models/Negociacao.js
@@ -1,6 +1,6 @@
     class Negociacao {
     constructor(data, quantidade, valor){
-        this._data = data
+        this._data = new Date(data.getTime())
         this._quantidade = quantidade
         this._valor = valor
         Object.freeze(this) //Congelando o objeto, deixando ele imutavel
@@ -10,7 +10,7 @@
         return this._quantidade*this.valor
     }
     get data(){
-        return this._data
+        return new Date(this._data.getTime())
     }
     get quantidade(){
         return this._quantidade
