5533a795387c1addb8afcf764d4c063b51273c15
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-10 17:35:23 -0300

    Melhorando data com arrow function e instanciando o objeto novamente

--- JS_4/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JS_4/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -12,12 +12,15 @@ class NegociacaoController{
         event.preventDefault()
 
         let data = new Date(
-            ...this._inputData.value.split('-').map(function(item,index){
-                if(index == 1)
-                    return item - 1
-                return item
-                })
+            ...this._inputData.value.split('-').map((item,index) => item - index%2)
         )
-        console.log(data)
+
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
+
+        console.log(negociacao)
     }
 }
