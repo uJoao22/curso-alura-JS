fe50f6d8ac9a3389da33e6b7ad5f3dde8a874c59
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-13 18:08:51 -0300

    Inserindo os dados de todas as semanas

--- JS_4/aluraframe/client/js/app/services/NegociacaoServices.js
+++ JS_4/aluraframe/client/js/app/services/NegociacaoServices.js
@@ -1,6 +1,6 @@
 class NegociacaoServices{
+    //USANDO AJAX COM JAVASCRIPT PURO
     obterNegociacoesDaSemana(cb){
-        //USANDO AJAX COM JAVASCRIPT PURO
         let xhr = new XMLHttpRequest() //Criando uma instancia de XMLHttp
 
         xhr.open('GET', 'negociacoes/semana') //Preparando para abrir o servidor XMLHttp no metodo GET para o endreço local negociacoes/semana
@@ -20,5 +20,43 @@ class NegociacaoServices{
         }
 
         xhr.send() //Eecutar a operação de abrir
+    }
+
+    obterNegociacoesDaSemanaAnterior(cb){
+        let xhr = new XMLHttpRequest()
+
+        xhr.open('GET', 'negociacoes/anterior')
+
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState == 4){
+                if(xhr.status == 200){
+                    cb(null, JSON.parse(xhr.responseText).map(objeto => new Negociacao(new Date(objeto.data),  objeto.quantidade, objeto.valor)))
+                } else {
+                    console.log(xhr.responseText)
+                    cb("Não foi possível obter as negociações")
+                }
+            }
+        }
+
+        xhr.send()
+    }
+
+    obterNegociacoesDaSemanaRetrasada(cb){
+        let xhr = new XMLHttpRequest()
+
+        xhr.open('GET', 'negociacoes/retrasada')
+
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState == 4){
+                if(xhr.status == 200){
+                    cb(null, JSON.parse(xhr.responseText).map(objeto => new Negociacao(new Date(objeto.data),  objeto.quantidade, objeto.valor)))
+                } else {
+                    console.log(xhr.responseText)
+                    cb("Não foi possível obter as negociações")
+                }
+            }
+        }
+
+        xhr.send()
     }
 }
