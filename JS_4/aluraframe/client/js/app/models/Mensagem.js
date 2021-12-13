0274d59b20181604c9c2f20992d13dfca786ef1c
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-13 13:34:22 -0300

    Criando a classe Mensagem para inserir uma mensagem de inserção

---
+++ JS_4/aluraframe/client/js/app/models/Mensagem.js
@@ -0,0 +1,13 @@
+class Mensagem{
+    constructor(texto=''){ //Atribuindo um valor padrão para o parametro do construtor
+        this._texto = texto;
+    }
+
+    get texto(){ //Usando o metodo get para visualizar o valor do objeto
+        return this._texto
+    }
+
+    set texto(texto){ //Usando o metodo set para alterar os dados
+        this._texto = texto
+    }
+}
