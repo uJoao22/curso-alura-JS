6c96e9a5995703b0ed43d2d238f424601320b4c8
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-13 13:07:00 -0300

    Inserindo os dados dinamicamente na tabela, usando template string

--- JS_4/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JS_4/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -8,16 +8,14 @@ class NegociacaoController{
         this._listaNegociacoes = new ListaNegociacoes()
         this._negociacoesView = new NegociacoesView($('#negociacoesView'))
 
-        this._negociacoesView.update() //Quando executar esta função a tabela devera ser incluida no DOM
+        this._negociacoesView.update(this._listaNegociacoes) //Quando executar esta função a tabela devera ser incluida no DOM
     }
 
     adiciona(event){
         event.preventDefault()
-
         this._listaNegociacoes.adiciona(this._criaNegociacao())
+        this._negociacoesView.update(this._listaNegociacoes) //Quando o metodo adicona for chamada, deverá executar o update na tabel
         this._limpaFormulario()
-
-        console.log(this._listaNegociacoes.negociacoes)
     }
 
     _criaNegociacao() {
@@ -33,7 +31,6 @@ class NegociacaoController{
         this._inputData.value = ''
         this._inputQuantidade.value = 1
         this._inputValor.value = 0.0
-
         this._inputData.focus()
     }
 }
--- JS_4/aluraframe/client/js/app/views/NegociacoesView.js
+++ JS_4/aluraframe/client/js/app/views/NegociacoesView.js
@@ -3,7 +3,7 @@ class NegociacoesView {
         this._elemento = elemento
     }
 
-    _template() {
+    _template(model) { //Recebendo os dados inseridos por parametro
         //Usando a template string para retornar a tabela com os dados
         return `
         <table class="table table-hover table-bordered">
@@ -17,6 +17,15 @@ class NegociacoesView {
             </thead>
 
             <tbody>
+                ${model.negociacoes.map(n => { //Mapeando os dados da negociação que está em formato de array para serem inseridos dinamicamente
+                    return `
+                        <tr>
+                            <td>${DateHelper.dataParaTexto(n.data)}</td>
+                            <td>${n.quantidade}</td>
+                            <td>${n.valor}</td>
+                            <td>${n.volume}</td>
+                        </tr>`
+                }).join('') /*Usando o join para concatenar todas as strings e retornar apenas uma string para o template string pai*/ }
             </tbody>
 
             <tfoot>
@@ -25,7 +34,7 @@ class NegociacoesView {
 
     }
 
-    update(){
-        this._elemento.innerHTML = this._template() //Quando o metodo update for chamado ele irá inserir a tabela na div #negociacoesView
+    update(model){ //Quando o metodo update for chamado ele irá inserir a tabela na div #negociacoesView e recebendo como parametro os dados a serem incluidos
+        this._elemento.innerHTML = this._template(model) //Inserir no DOM os dados do metodo _templade com o model como parametro
     }
 }
