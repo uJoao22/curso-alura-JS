6cb65beb3370ca5612395d3fb70239cee1a9e10c
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-13 13:22:44 -0300

    Usando o reduce para calcular e inserir o valor total

--- JS_4/aluraframe/client/js/app/views/NegociacoesView.js
+++ JS_4/aluraframe/client/js/app/views/NegociacoesView.js
@@ -29,12 +29,7 @@ class NegociacoesView {
 
             <tfoot>
                 <td colspan="3"></td>
-                <td>${
-                    (function(){ //Criando uma função autoinvocavel para executar blocos de código na template string
-                        let total = 0
-                        model.negociacoes.forEach(n => total+=n.volume)
-                        return total
-                    })()
+                <td>${model.negociacoes.reduce((total, n) => total+n.volume, 0.0) //Usando o reduce para transformar todos os dados do array de negociacoes em apenas um item, o valor total
                 }</td>
             </tfoot>
         </table> `
