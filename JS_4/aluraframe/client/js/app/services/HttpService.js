0d9ffa0891c8a7966c0d8f9e1740ccc7fd7f235e
joaoBotelho22 <joaopedro@saudeagora.com.vc>
2021-12-15 15:29:25 -0300

    Simplificando requisição usando Fetch API

--- JS_4/aluraframe/client/js/app/services/HttpService.js
+++ JS_4/aluraframe/client/js/app/services/HttpService.js
@@ -1,21 +1,32 @@
 class HttpService{
+    _handleErros(res){
+        if(!res.ok) throw new Error(res.statusTexxt) //Se der algum erro, ele irá lançar o erro
+        return res //Se estiver tudo ok ele retornar res
+    }
+
     get(url) {
-        return new Promise((resolve, reject) => {
-            let xhr = new XMLHttpRequest()
+        //Simplificando a requisição de dados usando Fetch API, recurso do ES2016
+        return fetch(url)
+            .then(res => this._handleErros(res)) //Chamando o metodo para tratar erros, se estive ok, siga
+            .then(res => res.json()) //Após tratamento de erros, retorne a resposta em formato JSON
 
-            xhr.open('GET', url)
 
-            xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4){
-                    if(xhr.status == 200){
-                        resolve(JSON.parse(xhr.responseText))
-                    } else {
-                        reject(xhr.responseText)
-                    }
-                }
-            }
+        // return new Promise((resolve, reject) => {
+        //     let xhr = new XMLHttpRequest()
 
-            xhr.send()
-        })
+        //     xhr.open('GET', url)
+
+        //     xhr.onreadystatechange = () => {
+        //         if(xhr.readyState == 4){
+        //             if(xhr.status == 200){
+        //                 resolve(JSON.parse(xhr.responseText))
+        //             } else {
+        //                 reject(xhr.responseText)
+        //             }
+        //         }
+        //     }
+
+        //     xhr.send()
+        // })
     }
 }
